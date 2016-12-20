e27ce6a6bbc1683fbd0b0f00dd3adb48ca2696b1
doc HD <doc.divxm@gmail.com>
2016-12-20 13:17:19 +0300

    ferrari: İnherit device.mk first and fix typo
    Change-Id: I7c2d2c0929fee282ce7391c35ddfb55283aa5926

--- full_ferrari.mk
+++ full_ferrari.mk
@@ -14,12 +14,12 @@
 # limitations under the License.
 #
 
+# Inherit from ferrari device
+$(call inherit-product, device/xiaomi/ferrari/device.mk)
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit from ferrari device
-$(call inherit-product, device/xiaomi/ferrari/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ferrari
